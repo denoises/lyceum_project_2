aac716d85249610fc749b8b6891d9c470352b469
D4N1L4 <69013717+D4N1L4@users.noreply.github.com>
2023-01-28 15:33:25 +0300

    1

--- main.py
+++ main.py
@@ -157,7 +157,7 @@ def move(hero, moves, level_map):
 
 
 def load_level(filename):
-    filename = "data/" + filename
+    filename = "image/low_settings/" + filename
     # читаем уровень, убирая символы перевода строки
     with open(filename, 'r') as mapFile:
         level_map = [line.strip() for line in mapFile]
